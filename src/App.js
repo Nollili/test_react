117741c900e60c0823aecd227b7c6cbaf4216f8b
Nollili <66601688+Nollili@users.noreply.github.com>
2021-05-28 16:37:52 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Todo from './components/Todo';
+
+
+
+function App() {
+	return (
+		<div>
+			<h1>My Todos</h1>
+      <Todo text = 'First task'/>
+      <Todo text = 'Second task'/>
+      <Todo text = 'Third task'/>
+		</div>
+	);
+}
+
+export default App;
